20e5902110ef583cca54ae0d5df70968bc5ed39f
IvanM <ivan.montes@outlook.com>
2024-02-17 10:29:22 +0100

    Initial commit

---
+++ src/main/java/dev/ime/desiremgmt/DesireMgmtApplication.java
@@ -0,0 +1,13 @@
+package dev.ime.desiremgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesireMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesireMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ime/desiremgmt/DesireMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ime.desiremgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesireMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
