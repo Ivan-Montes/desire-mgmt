3cde1996822e9d3088e70ef207bb403c9e861e63
IvanM <ivan.montes@outlook.com>
2024-02-21 13:57:53 +0100

    Create file with some constants

---
+++ ms-products/src/main/java/dev/ime/tool/SomeConstants.java
@@ -0,0 +1,11 @@
+package dev.ime.tool;
+
+public class SomeConstants {
+
+	private SomeConstants() {
+		super();
+	}
+	
+	public static final int SIZE_REQUEST = 5;
+	public static final String DATELESS = "N0 Data";
+}
