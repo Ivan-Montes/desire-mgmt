7389223334160264a71fc89c7ebfe98ba2c5c1e6
IvanM <ivan.montes@outlook.com>
2024-02-22 13:01:12 +0100

    Change settings

--- ms-products/src/main/java/dev/ime/tool/SomeConstants.java
+++ ms-products/src/main/java/dev/ime/tool/SomeConstants.java
@@ -8,4 +8,11 @@ public class SomeConstants {
 	
 	public static final int SIZE_REQUEST = 5;
 	public static final String DATELESS = "N0 Data";
+	
+	public static final String CATEGORYID = "CategoryId";
+	public static final String PRODUCTID = "ProductId";
+		
+	public static final String RESOURCE_NOT_FOUND = "ResourceNotFoundException";
+	
+	
 }
