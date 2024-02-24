4710ecd0fc1f1ceeeb89999a8b25951b22f3cd19
IvanM <ivan.montes@outlook.com>
2024-02-24 10:25:38 +0100

    Add repositories

--- ms-products/src/main/java/dev/ime/repository/CategoryRepository.java
+++ ms-products/src/main/java/dev/ime/repository/CategoryRepository.java
@@ -1,9 +1,12 @@
 package dev.ime.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.ime.entity.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
+	List<Category>findByName(String name);
 }
--- ms-products/src/main/java/dev/ime/repository/ProductRepository.java
+++ ms-products/src/main/java/dev/ime/repository/ProductRepository.java
@@ -1,9 +1,12 @@
 package dev.ime.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.ime.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+	List<Product>findByName(String name);
 }
