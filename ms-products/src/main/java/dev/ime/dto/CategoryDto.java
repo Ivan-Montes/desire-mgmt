61cc21c5b781371b818cac3f9c81860cfbac9db5
IvanM <ivan.montes@outlook.com>
2024-02-24 10:24:49 +0100

    Complete DTOs

--- ms-products/src/main/java/dev/ime/dto/CategoryDto.java
+++ ms-products/src/main/java/dev/ime/dto/CategoryDto.java
@@ -1,11 +1,13 @@
 package dev.ime.dto;
 
+import dev.ime.tool.RegexPattern;
 import dev.ime.tool.SomeConstants;
+import jakarta.validation.constraints.Pattern;
 
 public record CategoryDto(
 		Long categoryId,
-		String name,
-		String description
+		@Pattern( regexp = RegexPattern.NAME_FULL ) String name,
+		@Pattern( regexp = RegexPattern.DESCRIPTION_FULL )String description
 		) {
 	public CategoryDto() {
 		this(0L, SomeConstants.DATELESS, SomeConstants.DATELESS);
--- ms-products/src/main/java/dev/ime/dto/ProductDto.java
+++ ms-products/src/main/java/dev/ime/dto/ProductDto.java
@@ -1,13 +1,18 @@
 package dev.ime.dto;
 
+import dev.ime.tool.RegexPattern;
 import dev.ime.tool.SomeConstants;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record ProductDto(
 		Long productId,
-		String name,
-		Double unitPrice,
-		Integer unitInStock,
-		Boolean discontinued,
+		@Pattern( regexp = RegexPattern.NAME_FULL ) String name,
+		@NotNull@Min(0)@Max(99999) Double unitPrice,
+		@NotNull@Min(0)@Max(99999) Integer unitInStock,
+		@NotNull Boolean discontinued,
 		Long categoryId
 		) {
 
