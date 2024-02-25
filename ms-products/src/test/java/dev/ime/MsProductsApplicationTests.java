2b231ee916b5aee6009cfaa9b4216655213739e9
IvanM <ivan.montes@outlook.com>
2024-02-25 10:25:45 +0100

    Test for main class

--- ms-products/src/test/java/dev/ime/MsProductsApplicationTests.java
+++ ms-products/src/test/java/dev/ime/MsProductsApplicationTests.java
@@ -1,13 +1,15 @@
 package dev.ime;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest( classes = MsProductsApplication.class)
 class MsProductsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
