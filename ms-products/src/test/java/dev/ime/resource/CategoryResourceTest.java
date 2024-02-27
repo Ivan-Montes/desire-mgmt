0db6a4a9241838ef2c5d4decd0ff579ba39c58fa
IvanM <ivan.montes@outlook.com>
2024-02-27 09:30:24 +0100

    Tests for resource layer

---
+++ ms-products/src/test/java/dev/ime/resource/CategoryResourceTest.java
@@ -0,0 +1,262 @@
+package dev.ime.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.ime.dto.CategoryDto;
+import dev.ime.entity.Category;
+import dev.ime.mapper.CategoryMapper;
+import dev.ime.service.impl.CategoryServiceImpl;
+import dev.ime.tool.SomeConstants;
+
+@WebMvcTest(CategoryResource.class)
+@AutoConfigureMockMvc(addFilters = false)
+class CategoryResourceTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private CategoryServiceImpl categoryService;
+	
+	@MockBean
+	private  CategoryMapper categoryMapper;	
+	
+	@Autowired
+    private ObjectMapper objectMapper;
+	
+	private final String path = "/api/categories";
+	private List<CategoryDto>categoriesDtoS;
+	private List<Category>categories;
+	private CategoryDto catDto;
+	private Category catTest;
+	private final Long catId = 7L;
+	private final String catName = "Tesoros sagrados";
+	private final String catDescription = "Un Tesoro Sagrado o Shinki son espíritus que los dioses utilizan para diversas tareas y propósitos";
+	
+	@BeforeEach
+	private void createObjects() {
+		
+		categories = new ArrayList<>();
+		categoriesDtoS = new ArrayList<>();
+		
+		catDto = new CategoryDto(catId, catName, catDescription);
+		
+		catTest = new Category();
+		catTest.setId(catId);
+	}
+	
+	@Test
+	void CategoryResource_getAll_ReturnListEmpty() throws Exception{
+				
+		Mockito.when(categoryService.getAll()).thenReturn(categories);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path))
+		.andExpect(MockMvcResultMatchers.status().isNoContent())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.empty()))
+		;
+	}
+
+	@Test
+	void CategoryResource_getAll_ReturnListPagedDefaultSize() throws Exception{
+		
+		categories.add(catTest);
+		categoriesDtoS.add(catDto);
+		Mockito.when(categoryService.getAllPaged(Mockito.anyInt(), Mockito.anyInt())).thenReturn(categories);
+		Mockito.when(categoryMapper.toListCategoryDto(Mockito.anyList())).thenReturn(categoriesDtoS);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path)
+				.param("page", "1"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.hasSize(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].categoryId", org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", org.hamcrest.Matchers.equalTo(catName)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].description", org.hamcrest.Matchers.equalTo(catDescription)))
+		;
+	}
+	
+
+	@Test
+	void CategoryResource_getAll_ReturnListPaged() throws Exception{
+		
+		categories.add(catTest);
+		categoriesDtoS.add(catDto);
+		Mockito.when(categoryService.getAllPaged(Mockito.anyInt(), Mockito.anyInt())).thenReturn(categories);
+		Mockito.when(categoryMapper.toListCategoryDto(Mockito.anyList())).thenReturn(categoriesDtoS);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path)
+				.param("page", "1")
+				.param("size", "1"))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.hasSize(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].categoryId", org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", org.hamcrest.Matchers.equalTo(catName)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].description", org.hamcrest.Matchers.equalTo(catDescription)))
+		;
+	}
+	
+	@Test
+	void CategoryResource_getById_ReturnCategoryDto() throws Exception {
+		
+		Mockito.when(categoryService.getById(Mockito.anyLong())).thenReturn(Optional.of(catTest));
+		Mockito.when(categoryMapper.toCategoryDto(Mockito.any(Category.class))).thenReturn(catDto);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path + "/{id}", catId))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.categoryId",  org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(catName)))
+		;
+	}
+	
+	@Test
+	void CategoryResource_getById_ReturnCategoryDtoEmpty() throws Exception{
+		
+		Mockito.when(categoryService.getById(Mockito.anyLong())).thenReturn(Optional.empty());
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path + "/{id}", catId))
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.categoryId",  org.hamcrest.Matchers.equalTo(0)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(SomeConstants.DATELESS)))
+		;
+	}
+	
+
+	@Test
+	void CategoryResource_create_ReturnCategoryDto() throws Exception{
+		
+		Mockito.when(categoryService.create(Mockito.any(CategoryDto.class))).thenReturn(Optional.of(catTest));
+		Mockito.when(categoryMapper.toCategoryDto(Mockito.any(Category.class))).thenReturn(catDto);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(path)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(catDto))
+				)
+		.andExpect(MockMvcResultMatchers.status().isCreated())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(catName)))		
+		;
+	}
+
+	@Test
+	void CategoryResource_create_ReturnCategoryDtoEmpty() throws Exception{
+		
+		Mockito.when(categoryService.create(Mockito.any(CategoryDto.class))).thenReturn(Optional.empty());
+		
+		mockMvc.perform(MockMvcRequestBuilders.post(path)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(catDto))
+				)		
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.categoryId",  org.hamcrest.Matchers.equalTo(0)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(SomeConstants.DATELESS)))
+		;
+	}
+
+	@Test
+	void CategoryResource_update_ReturnCategoryDto() throws Exception{
+		
+		Mockito.when(categoryService.update(Mockito.anyLong(), Mockito.any(CategoryDto.class))).thenReturn(Optional.of(catTest));
+		Mockito.when(categoryMapper.toCategoryDto(Mockito.any(Category.class))).thenReturn(catDto);
+		
+		mockMvc.perform(MockMvcRequestBuilders.put(path + "/{id}", catId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(catDto))
+				)		
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.categoryId",  org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(catName)))		
+		;
+	}
+
+	@Test
+	void CategoryResource_update_ReturnCategoryDtoEmpty() throws Exception{
+		
+		Mockito.when(categoryService.update(Mockito.anyLong(), Mockito.any(CategoryDto.class))).thenReturn(Optional.empty());
+		
+		mockMvc.perform(MockMvcRequestBuilders.put(path + "/{id}", catId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(catDto))
+				)		
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.categoryId",  org.hamcrest.Matchers.equalTo(0)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(SomeConstants.DATELESS)))	
+		;
+	}
+	
+
+	@Test
+	void CategoryResource_delete_ReturnTrue() throws Exception{
+		
+		Mockito.when(categoryService.delete(Mockito.anyLong())).thenReturn(0);
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete(path + "/{id}", catId))	
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(true))		
+		;
+	}	
+
+	@Test
+	void CategoryResource_delete_ReturnFalse() throws Exception{
+		
+		Mockito.when(categoryService.delete(Mockito.anyLong())).thenReturn(1);
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete(path + "/{id}", catId))	
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(false))		
+		;
+	}
+
+	@Test
+	void CategoryResource_addProductToCategory_ReturnTrue() throws Exception{
+		
+		Mockito.when(categoryService.addProductToCategory(Mockito.anyLong(), Mockito.anyLong())).thenReturn(true);
+	
+		mockMvc.perform(MockMvcRequestBuilders.put(path +"/{categoryId}/products/{productId}", catId, catId))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(true))
+		;
+	}
+	
+
+	@Test
+	void CategoryResource_addProductToCategory_ReturnFalse() throws Exception{
+		
+		Mockito.when(categoryService.addProductToCategory(Mockito.anyLong(), Mockito.anyLong())).thenReturn(false);
+		
+		mockMvc.perform(MockMvcRequestBuilders.put(path +"/{categoryId}/products/{productId}", catId, catId))
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(false))
+		;
+	}
+}
---
+++ ms-products/src/test/java/dev/ime/resource/ProductResourceTest.java
@@ -0,0 +1,267 @@
+package dev.ime.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.ime.dto.ProductDto;
+import dev.ime.entity.Product;
+import dev.ime.mapper.ProductMapper;
+import dev.ime.service.impl.ProductServiceImpl;
+import dev.ime.tool.SomeConstants;
+
+@WebMvcTest(ProductResource.class)
+@AutoConfigureMockMvc(addFilters = false)
+class ProductResourceTest {
+	
+	@MockBean
+	private ProductServiceImpl productService;
+	
+	@MockBean
+	private ProductMapper productMapper;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
+	
+	private final String path = "/api/products";
+	private List<ProductDto>productsDtoS;
+	private List<Product>products;
+	private ProductDto proDto;
+	private Product proTest;
+	private final Long proId = 7L;
+	private final Long catId = 13L;
+	private final String proName = "Yukine";
+	private final Double unitPrice = 99.9;
+	private final Integer unitInStock = 17;
+	
+	@BeforeEach
+	private void createObjects() {
+		
+		productsDtoS = new ArrayList<>();
+		products = new ArrayList<>();
+		
+		proDto = new ProductDto(proId,proName,unitPrice,unitInStock,false,catId);
+		
+		proTest = new Product();
+		proTest.setId(proId);
+		proTest.setName(proName);
+		proTest.setUnitPrice(unitPrice);
+		proTest.setUnitInStock(unitInStock);
+		proTest.setDiscontinued(false);
+		
+	}
+	
+	@Test
+	void ProductResource_getAll_ReturnListEmpty() throws Exception {
+		Mockito.when(productService.getAll()).thenReturn(products);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path))
+		.andExpect(MockMvcResultMatchers.status().isNoContent())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.empty()))
+		;
+	}
+
+	@Test
+	void ProductResource_getAll_ReturnPagedList() throws Exception {
+		products.add(proTest);
+		productsDtoS.add(proDto);
+		Mockito.when(productService.getAllPaged(Mockito.anyInt(),Mockito.anyInt())).thenReturn(products);
+		Mockito.when(productMapper.toListProductDto(Mockito.anyList())).thenReturn(productsDtoS);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path)
+				.param("page", "1")
+				.param("size", "1")
+				)
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.hasSize(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].productId", org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", org.hamcrest.Matchers.equalTo(proName)))
+		;
+	}
+
+	@Test
+	void ProductResource_getAll_ReturnListPagedDefaultSize() throws Exception {
+		products.add(proTest);
+		productsDtoS.add(proDto);
+		Mockito.when(productService.getAllPaged(Mockito.anyInt(),Mockito.anyInt())).thenReturn(products);
+		Mockito.when(productMapper.toListProductDto(Mockito.anyList())).thenReturn(productsDtoS);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path)
+				.param("page", "1")
+				)
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.hasSize(1)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].productId", org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$[0].name", org.hamcrest.Matchers.equalTo(proName)))
+		;
+	}
+	
+
+	@Test
+	void ProductResource_getById_ReturnProductDto() throws Exception {
+		
+		Mockito.when(productService.getById(Mockito.anyLong())).thenReturn(Optional.of(proTest));
+		Mockito.when(productMapper.toProductDto(Mockito.any(Product.class))).thenReturn(proDto);
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path + "/{id}", proId))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.productId", org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(proName)))
+		;
+	}
+	
+	@Test
+	void ProductResource_getById_ReturnProductDtoEmpty() throws Exception {
+		
+		Mockito.when(productService.getById(Mockito.anyLong())).thenReturn(Optional.empty());
+		
+		mockMvc.perform(MockMvcRequestBuilders.get(path + "/{id}", proId))
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.productId", org.hamcrest.Matchers.equalTo(0)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(SomeConstants.DATELESS)))
+		;
+	}
+	
+
+	@Test
+	void ProductResource_create_ReturnProductDto() throws Exception{
+		
+		Mockito.when(productService.create(Mockito.any(ProductDto.class))).thenReturn(Optional.of(proTest));
+		Mockito.when(productMapper.toProductDto(Mockito.any(Product.class))).thenReturn(proDto);
+
+		mockMvc.perform(MockMvcRequestBuilders.post(path)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(proDto))
+				)
+		.andExpect(MockMvcResultMatchers.status().isCreated())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(proName)))		
+		;
+	}
+	
+
+	@Test
+	void ProductResource_create_ReturnProductDtoEmtpy() throws Exception{
+		
+		Mockito.when(productService.create(Mockito.any(ProductDto.class))).thenReturn(Optional.empty());
+
+		mockMvc.perform(MockMvcRequestBuilders.post(path)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(proDto))
+				)		
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.productId",  org.hamcrest.Matchers.equalTo(0)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(SomeConstants.DATELESS)))
+		;
+	}
+	
+	@Test
+	void ProductResource_update_ReturnProductDto() throws Exception{
+		
+		Mockito.when(productService.update(Mockito.anyLong(), Mockito.any(ProductDto.class))).thenReturn(Optional.of(proTest));
+		Mockito.when(productMapper.toProductDto(Mockito.any(Product.class))).thenReturn(proDto);
+
+		mockMvc.perform(MockMvcRequestBuilders.put(path + "/{id}", proId)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(proDto))
+				)
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.productId", org.hamcrest.Matchers.equalTo(7)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(proName)))		
+		;
+	}
+	
+
+	@Test
+	void ProductResource_update_ReturnProductDtoEmpty() throws Exception{
+		
+		Mockito.when(productService.update(Mockito.anyLong(), Mockito.any(ProductDto.class))).thenReturn(Optional.empty());
+		
+		mockMvc.perform(MockMvcRequestBuilders.put(path + "/{id}", proId)
+			.contentType(MediaType.APPLICATION_JSON)
+			.content(objectMapper.writeValueAsString(proDto))
+			)		
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$", org.hamcrest.Matchers.notNullValue()))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.productId",  org.hamcrest.Matchers.equalTo(0)))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.name", org.hamcrest.Matchers.equalTo(SomeConstants.DATELESS)))
+		;
+	}
+	
+
+	@Test
+	void ProductResource_delete_ReturnBooleanTrue() throws Exception {
+		
+		Mockito.when(productService.delete(Mockito.anyLong())).thenReturn(0);
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete(path + "/{id}", proId))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(true))	
+		;
+	}
+
+	@Test
+	void ProductResource_delete_ReturnBooleanFalse() throws Exception {
+		
+		Mockito.when(productService.delete(Mockito.anyLong())).thenReturn(1);
+		
+		mockMvc.perform(MockMvcRequestBuilders.delete(path + "/{id}", proId))
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(false))			
+		;
+	}
+	
+	@Test
+	void ProductResource_changeCategory_ReturnBooleanTrue() throws Exception {
+		
+		Mockito.when(productService.changeCategory(Mockito.anyLong(), Mockito.anyLong())).thenReturn(true);
+		
+		mockMvc.perform(MockMvcRequestBuilders.put(path + "/{id}/categories/{categoryId}", proId, catId))
+		.andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(true))	
+		;
+	}
+
+	@Test
+	void ProductResource_changeCategory_ReturnBooleanFalse() throws Exception {
+		
+		Mockito.when(productService.changeCategory(Mockito.anyLong(), Mockito.anyLong())).thenReturn(false);
+		
+		mockMvc.perform(MockMvcRequestBuilders.put(path + "/{id}/categories/{categoryId}", proId, catId))
+		.andExpect(MockMvcResultMatchers.status().isNotFound())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").exists())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").isBoolean())
+		.andExpect(MockMvcResultMatchers.jsonPath("$").value(false))	
+		;
+	}
+}
