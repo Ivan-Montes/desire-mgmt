73fa397e7ec968f71d31aeb15e49ae1542b8fa5b
IvanM <ivan.montes@outlook.com>
2024-02-29 10:51:03 +0100

    Develop interceptor for logging tasks

---
+++ ms-products/src/main/java/dev/ime/tool/GlobalHandlerInterceptorConfig.java
@@ -0,0 +1,22 @@
+package dev.ime.tool;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class GlobalHandlerInterceptorConfig implements WebMvcConfigurer {
+	 
+	 private final GlobalHandlerInterceptor interceptor; 	 
+	 
+	 public GlobalHandlerInterceptorConfig(GlobalHandlerInterceptor interceptor) {
+		super();
+		this.interceptor = interceptor;
+	}
+
+	@Override
+	    public void addInterceptors(InterceptorRegistry registry) {
+	        registry.addInterceptor(interceptor).addPathPatterns("/api/**");
+	    }
+}
