6563f50b6dce8b1b79439b486bb3f3da06e33ca0
IvanM <ivan.montes@outlook.com>
2024-03-01 08:51:17 +0100

    Add logger

--- ms-products/src/main/java/dev/ime/exception/GlobalExceptionHandler.java
+++ ms-products/src/main/java/dev/ime/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package dev.ime.exception;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +13,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import dev.ime.tool.SomeConstants;
 import io.swagger.v3.oas.annotations.Operation;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+	private final Logger logger;	
+	
+	public GlobalExceptionHandler(Logger logger) {
+		super();
+		this.logger = logger;
+	}
 
 	@Operation( summary = "Basic Exception", description = "Several implementations of it")
 	@ExceptionHandler({
@@ -24,15 +33,17 @@ public class GlobalExceptionHandler {
 			dev.ime.exception.AttributeUniqueException.class
 			})
 	public ResponseEntity<ExceptionResponse> basicException(BasicException ex){
-		
+
+		logger.severe("Threw " + ex.getName() + " :=: An implementation of Basic Exception Class");
 		return new ResponseEntity<>( new ExceptionResponse( ex.getIdentifier(),ex.getName(),ex.getDescription(),ex.getErrors() ),
 									HttpStatus.NOT_FOUND );
 	}	
 	
-	@Operation( summary = "methodArgumentNotValidException", description = "Some http request attribute has not fulfilled Jakarta validation")
+	@Operation( summary = SomeConstants.EX_METHOD_ARGUMENT_INVALID, description = "Some http request attribute has not fulfilled Jakarta validation")
 	@ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
 	public ResponseEntity<ExceptionResponse> methodArgumentNotValidException(MethodArgumentNotValidException ex){
-		
+
+		logger.severe("Throw " + SomeConstants.EX_METHOD_ARGUMENT_INVALID);
 		Map<String, String> errors = new HashMap<>();
 		    ex.getBindingResult().getAllErrors().forEach( error -> {
 		        String fieldName = ((FieldError) error).getField();
@@ -41,16 +52,17 @@ public class GlobalExceptionHandler {
 		    });
 		
 		return new ResponseEntity<>( new ExceptionResponse( UUID.randomUUID(),
-				"MethodArgumentNotValidException",
+				SomeConstants.EX_METHOD_ARGUMENT_INVALID,
 				"Think McFly, you got MethodArgumentNotValidException",
 				errors ),
 				HttpStatus.NOT_ACCEPTABLE );
 	}
 	
-	@Operation( summary = "methodArgumentTypeMismatchException", description = "This exception is thrown when method argument is not the expected type, for instance the id attribute in the path of an api request")
+	@Operation( summary = SomeConstants.EX_METHOD_ARGUMENT_TYPE, description = "This exception is thrown when method argument is not the expected type, for instance the id attribute in the path of an api request")
 	@ExceptionHandler(org.springframework.web.method.annotation.MethodArgumentTypeMismatchException.class)
 	public ResponseEntity<ExceptionResponse>methodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex){
-		
+
+		logger.severe("Lauch " + SomeConstants.EX_METHOD_ARGUMENT_TYPE);
 		String attrName = ex.getName();
 		String typeName = "Unknown type";				
 		Class<?> requiredType = ex.getRequiredType();
@@ -58,21 +70,22 @@ public class GlobalExceptionHandler {
 		if ( requiredType != null && requiredType.getName() != null ) typeName = requiredType.getName();
 		
 		return new ResponseEntity<>( new ExceptionResponse( UUID.randomUUID(),
-				"methodArgumentTypeMismatchException",
+				SomeConstants.EX_METHOD_ARGUMENT_TYPE,
 				"Think McFly, you got methodArgumentTypeMismatchException",
 				Map.of( attrName, typeName)  ),
 				HttpStatus.BAD_REQUEST );
 	}	
 	
-	@Operation( summary = "jakartaValidationConstraintViolationException", description = " Called by Hibernate after check class/attributes annotations")
+	@Operation( summary = SomeConstants.EX_JAKARTA_VAL, description = " Called by Hibernate after check class/attributes annotations")
 	@ExceptionHandler(
 		jakarta.validation.ConstraintViolationException.class
 		)		
 	public ResponseEntity<ExceptionResponse> jakartaValidationConstraintViolationException(Exception ex){
-		
+
+		logger.severe("Cast "+ SomeConstants.EX_JAKARTA_VAL);
 		return new ResponseEntity<>( new ExceptionResponse( UUID.randomUUID(),
-				"jakartaValidationConstraintViolationException",
-				"Think McFly, you got jakartaValidationConstraintViolationException",
+				SomeConstants.EX_JAKARTA_VAL,
+				"Whats wrong McFly?, you Chicken got jakartaValidationConstraintViolationException",
 				Map.of( ex.getLocalizedMessage(), ex.getMessage())  ),
 				HttpStatus.BAD_REQUEST );
 	}	
