a0dcd75f763184623e7ab0672b3f444ead43c2cf
IvanM <ivan.montes@outlook.com>
2024-03-03 10:09:28 +0100

    Mod settings

--- src/test/java/dev/ime/desiremgmt/DesireMgmtApplicationTests.java
+++ src/test/java/dev/ime/desiremgmt/DesireMgmtApplicationTests.java
@@ -1,13 +1,15 @@
 package dev.ime.desiremgmt;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = DesireMgmtApplication.class)
 class DesireMgmtApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertTrue(true);
 	}
 
 }
