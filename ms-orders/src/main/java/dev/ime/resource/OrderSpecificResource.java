92a4a729f328d3a2fd1d75f567b9c527111ebbf1
IvanM <ivan.montes@outlook.com>
2024-03-06 15:53:18 +0100

    Add new method to Order resource interface

--- ms-orders/src/main/java/dev/ime/resource/OrderSpecificResource.java
+++ ms-orders/src/main/java/dev/ime/resource/OrderSpecificResource.java
@@ -1,9 +1,13 @@
 package dev.ime.resource;
 
+import java.util.Set;
+
 import org.springframework.http.ResponseEntity;
+
+import dev.ime.dto.OrderDetailDto;
 
 public interface OrderSpecificResource {
 
 	ResponseEntity<Boolean> addOrderDetail(Long orderId, Long orderDetailId);
-	
+	ResponseEntity<Set<OrderDetailDto>> getLinesByOrderId(Long orderId);
 }
