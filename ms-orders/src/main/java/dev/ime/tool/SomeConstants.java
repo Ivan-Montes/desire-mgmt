2b2d9acfdf652eb2a9ac1c3dc958ecde51920ab2
IvanM <ivan.montes@outlook.com>
2024-03-08 08:58:18 +0100

    Add new and organize constants

--- ms-orders/src/main/java/dev/ime/tool/SomeConstants.java
+++ ms-orders/src/main/java/dev/ime/tool/SomeConstants.java
@@ -7,7 +7,9 @@ public class SomeConstants {
 	}
 	
 	public static final int SIZE_REQUEST = 5;
-	public static final String DATELESS = "N0 Data";
+	public static final String DATELESS = "N0 Data";	
+
+	public static final String FORMAT_TIME = "%1$TH:%1$TM:%1$TS";
 	
 	public static final String CATEGORYID = "CategoryId";
 	public static final String PRODUCTID = "ProductId";
@@ -15,14 +17,17 @@ public class SomeConstants {
 	public static final String ORDERID = "OrderId";
 	public static final String ORDERDETAILID = "OrderDetailId";
 	public static final String DATEFORMAT = "Date attribute";
-	public static final String CUSTOMERID = "CustomerId";
-		
-	public static final String RESOURCE_NOT_FOUND = "ResourceNotFoundException";	
-
-	public static final String FORMAT_TIME = "%1$TH:%1$TM:%1$TS";
+	public static final String CUSTOMERID = "CustomerId";		
 	
-	public static final String EX_METHOD_ARGUMENT_INVALID = "Method Argument Not Valid Exception";
-	public static final String EX_METHOD_ARGUMENT_TYPE = "Method Argument Type Mismatch Exception";
-	public static final String EX_JAKARTA_VAL= "Jakarta Validation Constraint Violation Exception";
+	public static final String ANYBODYHOME = "Hello, hello, anybody home?, Think McFly, think";
+
+	public static final String EX_RESOURCE_NOT_FOUND = "ResourceNotFoundException";	
+	public static final String EX_ENTITY_ASSOCIATED = "EntityAssociatedException";	
+	public static final String EX_DATE_BAD = "DateBadFormatException";
+	public static final String EX_ATTR_UNIQUE = "AttributeUniqueException";
+	public static final String EX_METHOD_ARGUMENT_INVALID = "MethodArgumentNotValidException";
+	public static final String EX_METHOD_ARGUMENT_TYPE = "MethodArgumentTypeMismatchException";
+	public static final String EX_JAKARTA_VAL= "ConstraintViolationException";
+	
 	
 }
