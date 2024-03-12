bcc372722040868b228b47c64c902866620d39e5
IvanM <ivan.montes@outlook.com>
2024-03-12 15:56:55 +0100

    Add more resilience4j safeguards

--- ms-orders/src/main/java/dev/ime/client/impl/MsProductsClientImpl.java
+++ ms-orders/src/main/java/dev/ime/client/impl/MsProductsClientImpl.java
@@ -9,7 +9,10 @@ import org.springframework.stereotype.Component;
 import dev.ime.client.MsProductsClient;
 import dev.ime.dto.ProductDto;
 import io.vavr.control.Try;
+import io.github.resilience4j.bulkhead.Bulkhead;
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.retry.Retry;
 
 @Component
 public class MsProductsClientImpl implements MsProductsClient{
@@ -22,16 +25,20 @@ public class MsProductsClientImpl implements MsProductsClient{
 	}
 	
 	@Override
-	public ResponseEntity<ProductDto> getProductById(Long productId) {
+	public ResponseEntity<ProductDto> getProductById(Long id) {
 		
-		CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("defaultName");
-    	
-    	Supplier<ResponseEntity<ProductDto>> decoratedSupplier = CircuitBreaker
-    		    .decorateSupplier(circuitBreaker, ()-> msProductsClient.getProductById(productId));
+		CircuitBreaker circuitBreaker = CircuitBreaker.ofDefaults("cbDef");
+		Retry retry = Retry.ofDefaults("rDef");
+		Bulkhead bulkhead = Bulkhead.ofDefaults("bhDef");
+		Supplier<ResponseEntity<ProductDto>> supplier = () -> msProductsClient.getProductById(id) ;
 
-    	return Try.ofSupplier(decoratedSupplier)
-    		    .recover(throwable -> new ResponseEntity<>(new ProductDto(),HttpStatus.NOT_FOUND))
-    		    .get();
+		Supplier<ResponseEntity<ProductDto>> decoratedSupplier = Decorators.ofSupplier(supplier)
+		  .withCircuitBreaker(circuitBreaker)
+		  .withBulkhead(bulkhead)
+		  .withRetry(retry)
+		  .decorate();
+
+		return Try.ofSupplier(decoratedSupplier).recover(t -> new ResponseEntity<>(new ProductDto(),HttpStatus.NOT_FOUND) ).get();		
 	}	
 
 }
