7590ba6a241d926e90827b036fb889e087253672
IvanM <ivan.montes@outlook.com>
2024-03-15 09:02:04 +0100

    Add tests for mapper class

---
+++ ms-customers/src/test/java/dev/ime/mapper/impl/AddressMapperTest.java
@@ -0,0 +1,90 @@
+package dev.ime.mapper.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.ime.dto.AddressDto;
+import dev.ime.entity.Address;
+import dev.ime.entity.Customer;
+
+
+@ExtendWith(MockitoExtension.class)
+class AddressMapperTest {
+
+	@InjectMocks
+	private AddressMapper addressMapper;
+	
+	private List<Address>addresses;
+	
+	private Address addressTest;
+	private final Long addressId = 4L;
+	private final String location = "Cattedrale di Santa Maria del Fiore";
+	private final String city = "Florence";
+	private final String country = "Italy";
+	private final String email = "florence@florence.it";
+
+	private AddressDto addressTestDto;
+	private final Long addressIdDto = 5L;
+	private final String locationDto = "San Marco 1";
+	private final String cityDto = "Venecia";
+	private final String countryDto = "Veneto";
+	private final String emailDto = "ven@ven.it";
+	private final Long customerId = 47L;
+	
+	@BeforeEach	
+	private void createObjects() {
+		
+		addresses = new ArrayList<>();
+		
+		addressTest = new Address();
+		addressTest.setId(addressId);
+		addressTest.setLocation(location);
+		addressTest.setCity(city);
+		addressTest.setCountry(country);
+		addressTest.setEmail(email);
+		addressTest.setCustomer(new Customer());
+		
+		addressTestDto = new AddressDto(addressIdDto,locationDto,cityDto,countryDto,emailDto,customerId);
+	
+	}
+	
+	@Test
+	void AddressMapper_fromDto_ReturnAddress() {
+		
+		Address address = addressMapper.fromDto(addressTestDto);
+		
+		org.junit.jupiter.api.Assertions.assertAll(
+				()-> Assertions.assertThat(address).isNotNull(),
+				()-> Assertions.assertThat(address.getId()).isEqualTo(addressIdDto),
+				()-> Assertions.assertThat(address.getLocation()).isEqualTo(locationDto),
+				()-> Assertions.assertThat(address.getCity()).isEqualTo(cityDto),
+				()-> Assertions.assertThat(address.getCountry()).isEqualTo(countryDto),
+				()-> Assertions.assertThat(address.getEmail()).isEqualTo(emailDto)
+				);
+	}
+
+	@Test
+	void AddressMapper_toListDto_ReturnListAddressDto() {
+		
+		addresses.add(addressTest);
+		
+		List<AddressDto>list = addressMapper.toListDto(addresses);
+		
+		org.junit.jupiter.api.Assertions.assertAll(
+				()-> Assertions.assertThat(list).isNotNull(),
+				()-> Assertions.assertThat(list).hasSize(1),
+				()-> Assertions.assertThat(list.get(0).id()).isEqualTo(addressId),
+				()-> Assertions.assertThat(list.get(0).location()).isEqualTo(location),
+				()-> Assertions.assertThat(list.get(0).city()).isEqualTo(city),
+				()-> Assertions.assertThat(list.get(0).country()).isEqualTo(country),
+				()-> Assertions.assertThat(list.get(0).email()).isEqualTo(email)
+				);
+	}
+}
---
+++ ms-customers/src/test/java/dev/ime/mapper/impl/CustomerMapperTest.java
@@ -0,0 +1,76 @@
+package dev.ime.mapper.impl;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.ime.dto.CustomerDto;
+import dev.ime.entity.Customer;
+
+@ExtendWith(MockitoExtension.class)
+class CustomerMapperTest {
+
+	@InjectMocks
+	private CustomerMapper customerMapper;
+
+	private List<Customer>customers;
+	private List<CustomerDto>customersDtos;
+	private Customer customerTest;
+	private CustomerDto customerTestDto;
+	private final Long customerId = 4L;
+	private final String companyName = "Konohagakure";
+	private final String contactName = "Kakashi";
+	
+	@BeforeEach	
+	private void createObjects() {
+		
+		customers = new ArrayList<>();
+		customersDtos = new ArrayList<>();
+		
+		customerTest = new Customer();
+		customerTest.setId(customerId);
+		customerTest.setCompanyName(companyName);
+		customerTest.setContactName(contactName);
+		
+		customerTestDto = new CustomerDto(customerId, companyName, contactName);		
+		
+	}
+	
+	@Test
+	void AddressMapper_fromDto_ReturnAddress() {
+		
+		Customer customer = customerMapper.fromDto(customerTestDto);
+		
+		org.junit.jupiter.api.Assertions.assertAll(
+				()-> Assertions.assertThat(customer).isNotNull(),
+				()-> Assertions.assertThat(customer.getId()).isEqualTo(customerId),
+				()-> Assertions.assertThat(customer.getCompanyName()).isEqualTo(companyName),
+				()-> Assertions.assertThat(customer.getContactName()).isEqualTo(contactName)
+				);
+	}
+
+	@Test
+	void AddressMapper_toListDto_ReturnListCustomerDto() {
+		
+		customers.add(customerTest);
+		customersDtos.add(customerTestDto);
+		
+		List<CustomerDto>list = customerMapper.toListDto(customers);
+	
+		org.junit.jupiter.api.Assertions.assertAll(
+				()-> Assertions.assertThat(list).isNotNull(),
+				()-> Assertions.assertThat(list).hasSize(1),
+				()-> Assertions.assertThat(list.get(0).id()).isEqualTo(customerId.intValue()),
+				()-> Assertions.assertThat(list.get(0).companyName()).isEqualTo(companyName),
+				()-> Assertions.assertThat(list.get(0).contactName()).isEqualTo(contactName)
+				);
+	}
+	
+}
