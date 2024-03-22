7c3f584323b99fd4547b11961fd9b4016f65513a
IvanM <ivan.montes@outlook.com>
2024-03-22 10:58:20 +0100

    Add pattern location

--- ms-customers/src/main/java/dev/ime/tool/RegexPattern.java
+++ ms-customers/src/main/java/dev/ime/tool/RegexPattern.java
@@ -11,4 +11,5 @@ public class RegexPattern {
 	public static final String DESCRIPTION_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:]{0,100}";
 	public static final String LOCALDATE = "^\\d{4}-\\d{2}-\\d{2}$";
 	public static final String LOCATION_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:/ªº()]{0,100}";
+
 }
--- ms-products/src/main/java/dev/ime/tool/RegexPattern.java
+++ ms-products/src/main/java/dev/ime/tool/RegexPattern.java
@@ -9,5 +9,7 @@ public class RegexPattern {
 	public static final String NAME_BASIC = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.,&:]+";
 	public static final String NAME_FULL = "^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{0,50}";
 	public static final String DESCRIPTION_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:]{0,100}";
-	public static final String LOCALDATE = "^\\d{4}-\\d{2}-\\d{2}$";
+	public static final String LOCALDATE = "^\\d{4}-\\d{2}-\\d{2}$";	
+	public static final String LOCATION_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:/ªº()]{0,100}";
+
 }
