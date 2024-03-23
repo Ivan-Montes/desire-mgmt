31c2bbc8a701cdab2610185f3d8c2fbb2e05719a
IvanM <ivan.montes@outlook.com>
2024-03-23 09:05:47 +0100

    Import tool package

---
+++ ms-ui/src/main/java/dev/ime/tool/SomeConstants.java
@@ -0,0 +1,38 @@
+package dev.ime.tool;
+
+public class SomeConstants {
+
+	private SomeConstants() {
+		super();
+	}
+	
+	public static final int SIZE_REQUEST = 5;
+	public static final String DATELESS = "No Data";
+
+	public static final String FORMAT_TIME = "%1$TH:%1$TM:%1$TS";
+	
+	public static final String CATEGORYID = "CategoryId";
+	public static final String PRODUCTID = "ProductId";
+	public static final String NAMEATTR = "Name Attribute";
+	public static final String ORDERID = "OrderId";
+	public static final String ORDERDETAILID = "OrderDetailId";		
+	public static final String DATEFORMAT = "Date attribute";
+	public static final String CUSTOMERID = "CustomerId";		
+	public static final String ADDRESSID = "AddressId";		
+
+
+	public static final String ANYBODYHOME = "Hello, hello, anybody home?, Think McFly, think";
+
+	public static final String EX_RESOURCE_NOT_FOUND = "ResourceNotFoundException";	
+	public static final String EX_ENTITY_ASSOCIATED = "EntityAssociatedException";	
+	public static final String EX_DATE_BAD = "DateBadFormatException";
+	public static final String EX_ATTR_UNIQUE = "AttributeUniqueException";	
+	public static final String EX_METHOD_ARGUMENT_INVALID = "MethodArgumentNotValidException";
+	public static final String EX_METHOD_ARGUMENT_TYPE = "MethodArgumentTypeMismatchException";
+	public static final String EX_JAKARTA_VAL= "ConstraintViolationException";
+	
+	public static final String R4_CB = "cbDef";
+	public static final String R4_R = "rDef";
+	public static final String R4_BH = "bhDef";
+	
+}
