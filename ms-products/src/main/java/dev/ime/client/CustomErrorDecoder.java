f1fc7405e00e3e8114a62b1995e229220ea0c52b
IvanM <ivan.montes@outlook.com>
2024-03-27 10:27:50 +0100

    Add CustomErrorDecoder, ClienConfig and edit sql file

---
+++ ms-products/src/main/java/dev/ime/client/CustomErrorDecoder.java
@@ -0,0 +1,74 @@
+package dev.ime.client;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dev.ime.exception.*;
+import dev.ime.exceptionresponse.ExceptionResponse;
+import dev.ime.tool.SomeConstants;
+import feign.Response;
+import feign.codec.ErrorDecoder;
+
+
+public class CustomErrorDecoder implements ErrorDecoder {
+		
+	private final ObjectMapper objectMapper;	
+	private final Logger logger;
+	
+	public CustomErrorDecoder(ObjectMapper objectMapper, Logger logger) {
+		super();
+		this.objectMapper = objectMapper;
+		this.logger = logger;
+	}
+
+	@Override
+	public Exception decode(String methodKey, Response response) {
+		
+		ExceptionResponse exResponse = null;
+		
+		try {
+			exResponse = objectMapper.readValue(
+							response.body().asInputStream(),
+							ExceptionResponse.class
+						);
+			
+			if (exResponse != null) {
+				
+				switch(exResponse.name()) {
+				
+				case SomeConstants.EX_RESOURCE_NOT_FOUND:
+					return new ResourceNotFoundException(exResponse.error());
+					
+				case SomeConstants.EX_ENTITY_ASSOCIATED:
+					return new EntityAssociatedException(exResponse.error());
+					
+				case SomeConstants.EX_ATTR_UNIQUE:
+					return new AttributeUniqueException(exResponse.error());
+					
+				default:
+					return new BasicException(
+									UUID.randomUUID(), 
+									exResponse.name(), 
+									exResponse.description(), 
+									exResponse.error());									
+				}
+
+			}
+		
+		} catch (Exception e) {
+			logger.severe("Throw Exception in CustomErrorDecoder");
+		}		
+		
+		Map<String,String> errorMap = new HashMap<>();
+		errorMap.put("MethodKey", methodKey);
+		errorMap.put("Status",String.valueOf(response.status()));
+		errorMap.put("Url", response.request().url());
+		
+		return new BasicException(UUID.randomUUID(), "BasicException", "", errorMap);
+	}
+
+}
