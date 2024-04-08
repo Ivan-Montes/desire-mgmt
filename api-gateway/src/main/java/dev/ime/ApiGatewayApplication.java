49bf08771c605ed1a8562cb0ab2644a4e798fb5d
IvanM <ivan.montes@outlook.com>
2024-04-08 08:26:38 +0200

    feat: add openapi dependency and meta info

--- api-gateway/src/main/java/dev/ime/ApiGatewayApplication.java
+++ api-gateway/src/main/java/dev/ime/ApiGatewayApplication.java
@@ -1,9 +1,24 @@
 package dev.ime;
 
+
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.*;
+
+
+@OpenAPIDefinition (info =
+@Info(
+          title = "api-gateway",
+          version = "1.0",
+          description = "API api-gateway",
+          license = @License(name = "GNU GPLv3", url = "https://choosealicense.com/licenses/gpl-3.0/"),
+          contact = @Contact(url = "https://github.com/Ivan-Montes", name = "IvanM", email = "ivan@github.com")
+  )
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
@@ -11,5 +26,5 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
-
+	
 }
