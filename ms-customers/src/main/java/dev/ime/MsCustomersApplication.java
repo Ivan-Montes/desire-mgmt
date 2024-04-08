c31637f42f7c623a6f669d590d0167dbea670ffe
IvanM <ivan.montes@outlook.com>
2024-04-08 08:28:02 +0200

    feat: add Open Api definition en main class

--- ms-customers/src/main/java/dev/ime/MsCustomersApplication.java
+++ ms-customers/src/main/java/dev/ime/MsCustomersApplication.java
@@ -4,6 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition (info =
+@Info(
+          title = "ms-customers",
+          version = "1.0",
+          description = "API ms-customers",
+          license = @License(name = "GNU GPLv3", url = "https://choosealicense.com/licenses/gpl-3.0/"),
+          contact = @Contact(url = "https://github.com/Ivan-Montes", name = "IvanM", email = "ivan@github.com")
+  )
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 public class MsCustomersApplication {
--- ms-orders/src/main/java/dev/ime/MsOrdersApplication.java
+++ ms-orders/src/main/java/dev/ime/MsOrdersApplication.java
@@ -5,6 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition (info =
+@Info(
+          title = "ms-orders",
+          version = "1.0",
+          description = "API ms-orders",
+          license = @License(name = "GNU GPLv3", url = "https://choosealicense.com/licenses/gpl-3.0/"),
+          contact = @Contact(url = "https://github.com/Ivan-Montes", name = "IvanM", email = "ivan@github.com")
+  )
+)
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
