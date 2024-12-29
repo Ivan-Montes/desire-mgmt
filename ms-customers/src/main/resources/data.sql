c86786e7b3ed21da0622820edd1642f4f9f90a84
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-29 09:19:36 +0100

    chore: rewrite info of an insert sentence

--- ms-customers/src/main/resources/data.sql
+++ ms-customers/src/main/resources/data.sql
@@ -8,7 +8,7 @@ VALUES('Tyrell Corporation','Eldon Tyrell', CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)
 INSERT INTO addresses(location,city,country,email,creation_timestamp,update_timestamp,customer_id)
 VALUES('Sinister skyscrapper 121 st','Neo Tokyo','Nippon','tyrell@tyrell.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,1),
 ('Orohena volcano','Tahiti','French Polynesia','vile@vile.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,2),
-('comercial distric 71','Tanganika','Congo','rekall@rekall.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,3),
+('Comercial distric 71','Tanganika','Congo','rekall@rekall.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,3),
 ('3st Ave','Detroit','USA','ocp@ocp.com',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,4),
 ('Eddie Valiant Pz','Toontown','Planet Toons','acme@acme.tt',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,5),
 ('Sunset Bulevard 87','Hollywood','USA','acme@acme.us',CURRENT_TIMESTAMP,CURRENT_TIMESTAMP,5),
