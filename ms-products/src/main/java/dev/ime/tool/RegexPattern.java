4db4d882f60d2e566a0341b26a29714c6d04b9a5
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-30 09:39:32 +0100

    refactor: improve RegexPattern constructor

--- ms-products/src/main/java/dev/ime/tool/RegexPattern.java
+++ ms-products/src/main/java/dev/ime/tool/RegexPattern.java
@@ -1,11 +1,11 @@
 package dev.ime.tool;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+public final class RegexPattern {
 
-@NoArgsConstructor( access= AccessLevel.PRIVATE )
-public class RegexPattern {
-
+	private RegexPattern() {
+		super();
+	}
+	
 	public static final String NAME_BASIC = "[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.,&:]+";
 	public static final String NAME_FULL = "^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+[a-zA-ZñÑáéíóúÁÉÍÓÚ\\s\\-\\.&,:]{0,50}";
 	public static final String DESCRIPTION_FULL = "[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ\\s\\-\\?\\¿\\!\\¡\\.&,:]{0,100}";
