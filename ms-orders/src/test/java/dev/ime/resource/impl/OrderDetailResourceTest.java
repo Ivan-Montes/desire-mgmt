cb91dc5d7eff384062d8200dbe46625b5f1f6de4
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-01-03 07:36:03 +0100

    test: add @MockitoBean annotation

--- ms-orders/src/test/java/dev/ime/resource/impl/OrderDetailResourceTest.java
+++ ms-orders/src/test/java/dev/ime/resource/impl/OrderDetailResourceTest.java
@@ -13,7 +13,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -34,16 +34,16 @@ class OrderDetailResourceTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
+	@MockitoBean
 	private OrderDetailServiceImpl orderDetailService;
 	
-	@MockBean
+	@MockitoBean
 	private OrderDetailMapper orderDetailMapper;	
 
 	@Autowired
     private ObjectMapper objectMapper;
 	
-	@MockBean
+	@MockitoBean
 	private Logger logger;
 	
 	private final String path = "/api/orderdetails";
--- ms-orders/src/test/java/dev/ime/resource/impl/OrderResourceTest.java
+++ ms-orders/src/test/java/dev/ime/resource/impl/OrderResourceTest.java
@@ -15,7 +15,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -38,19 +38,19 @@ class OrderResourceTest {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	@MockBean
+	@MockitoBean
 	private OrderServiceImpl orderService;
 	
-	@MockBean
+	@MockitoBean
 	private OrderMapper orderMapper;
 	
-	@MockBean
+	@MockitoBean
 	private OrderDetailMapper orderDetailMapper;
 	
 	@Autowired
     private ObjectMapper objectMapper;
 	
-	@MockBean
+	@MockitoBean
 	private Logger logger;
 	
 	private final String path = "/api/orders";
