5a8b6f95292c92dc60975d560f171bc20cae1edf
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-01-05 09:22:20 +0100

    test: replace tag in the near future deprecated

--- ms-ui/src/test/java/dev/ime/controller/MainControllerTest.java
+++ ms-ui/src/test/java/dev/ime/controller/MainControllerTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -22,10 +22,10 @@ class MainControllerTest {
 	@Autowired
 	private MockMvc mockMvc;	
 
-	@MockBean
+	@MockitoBean
 	private GlobalExceptionHandler globalExceptionHandler;
 	
-	@MockBean
+	@MockitoBean
 	private Logger logger;
 	
 	@Test
